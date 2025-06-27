ed5a571c04fd034171408a07129a706aebcb06cc
vitoriavivi <146108876+vitoriavivi@users.noreply.github.com>
2025-06-27 09:41:39 -0300

    aula

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const botores - document.querySelectorAll["botao"];
+const textos = document.querySelector(".abas-conteudo")
 console (botoes)
 
 for(i=0;i<botoes.length; i++){
+    i].onclick = function
     botoes[i].onclick = function(){
         for(j=0; j<botoes.length; j++){
             botoes[j].classList.renove(ativo)
